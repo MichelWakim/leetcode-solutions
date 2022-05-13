c28728c07fe7d7d708deefe1ebafe78f9b45f54e
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-13 12:49:54 +0200

    Time: 1 ms (44.57%), Space: 41.5 MB (67.66%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -2,13 +2,8 @@ class Solution {
     public int balancedStringSplit(String s) {
         int result = 0, sum = 0;
         
-        // for (char letter : s.toCharArray()) {
-        //     sum += (letter == 'R'? 1 : -1);
-        //     result += (sum == 0? 1: 0);
-        // }
-        
-        for (int i = 0; i < s.length(); i++) {
-            sum += (s.charAt(i) == 'R'? 1 : -1);
+        for (char letter : s.toCharArray()) {
+            sum += (letter == 'R'? 1 : -1);
             result += (sum == 0? 1: 0);
         }
         
