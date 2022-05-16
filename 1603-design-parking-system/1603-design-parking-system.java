13bfa520e798477e985e5daf35e801b8e0d48881
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-16 15:25:25 +0200

    Time: 17 ms (6.73%), Space: 53.9 MB (67.44%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,36 +1,12 @@
 class ParkingSystem {
-    private int big = 0,
-                medium = 0,
-                small = 0;
+    private int[] count;
 
     public ParkingSystem(int big, int medium, int small) {
-        this.big = big;
-        this.medium = medium;
-        this.small = small;
+        count = new int[]{big, medium, small};
     }
     
     public boolean addCar(int carType) {
-        switch (carType) {
-            case 1 :
-                if(this.big > 0) {
-                    this.big --;
-                    return true;
-                }
-                break;
-            case 2 :
-                if(this.medium > 0) {
-                    this.medium --;
-                    return true;
-                }
-                break;
-            case 3 :
-                if(this.small > 0) {
-                    this.small --;
-                    return true;
-                }
-                break;
-        }
-        return false;
+       return count[--carType] -- > 0 ;
     }
 }
 
