a951235fc763fd23293c86f8444860af93c29c37
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-18 12:08:47 +0200

    Time: 7 ms (93.11%), Space: 44.9 MB (97.25%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -3,14 +3,13 @@ class NumArray {
     private int[] sum;
     
     public NumArray(int[] nums) {
-        sum = nums;
-        for (int i = 1; i < nums.length; i++) {
-            sum[i] += sum[i - 1];
-        }
+       sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) 
+            sum[i + 1] = sum[i] + nums[i];
     }
     
-    public int sumRange(int left, int right) {
-         return left == 0 ? sum[right] : sum[right] - sum[left - 1];
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
     }
 }
 
