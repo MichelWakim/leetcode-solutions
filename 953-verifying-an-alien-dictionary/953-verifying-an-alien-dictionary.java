54d285eb75aa5e8d086c74fd65da15a185da3a63
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-22 09:12:18 +0200

    Time: 1 ms (69.48%), Space: 42.3 MB (63.47%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -18,6 +18,7 @@ class Solution {
                     break;
                 } 
             }
+            
             if(same && a.length() > b.length()){
                return false;
             }
