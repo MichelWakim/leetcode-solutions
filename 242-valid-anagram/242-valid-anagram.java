6b65241f58aeef1574fd23dd9f0c81d254241816
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-22 21:27:30 +0200

    Time: 3 ms (91.67%), Space: 42.4 MB (87.54%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        
         int[] charsMap = new int['z'-'a'+1];
-    
-        for(char c: s.toCharArray()) {
-            int pos = c - 'a';
-            charsMap[pos]++;
-        }
 
-        for(char c: t.toCharArray()) {
-            int pos = c - 'a';
-            charsMap[pos]--;
+        for(int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            charsMap[idx] ++;
+            idx = t.charAt(i) - 'a';
+            charsMap[idx] --;
         }
 
         for(int count: charsMap) {
