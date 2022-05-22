881637cb5df8e6b875eeebca174eb236798fe9a6
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-22 09:48:30 +0200

    Time: 1 ms (97.90%), Space: 51.3 MB (25.66%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        /** 
+            Explaination :
+            https://www.youtube.com/watch?v=XzQSPg6LFyY
+        */
+        int result = 0;
+        for(int num : nums) result ^= num;
+        return result;
+    }
+}
