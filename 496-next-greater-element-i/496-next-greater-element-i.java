fcc557749636ef9d60ea0412f7eed8a2d321e1db
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-24 18:51:58 +0200

    Time: 5 ms (57.64%), Space: 43.8 MB (66.07%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -2,15 +2,22 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
        int[] ans = new int[nums1.length];
 
-        for(int i = 0; i < nums1.length; i++) {
-            int greaterIdx = -1, j = nums2.length - 1;
-            while(j >= 0 && nums2[j] != nums1[i]) {
-                if(nums2[j] > nums1[i]){
-                    greaterIdx = nums2[j];
-                }
-                j--;
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // find out all the next greater elements in nums2 array
+        for(int num: nums2) {
+            // if num is greater than top elements in stack then it is the next greater element in nums2
+            while(!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
             }
-            ans[i] = greaterIdx;
+            // then add num to stack
+            stack.add(num);
+        }
+
+        int i = 0;
+        for(int num : nums1) {
+            ans[i++] = map.getOrDefault(num, -1);
         }
 
         return ans;
