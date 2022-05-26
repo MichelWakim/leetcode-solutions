878e60fbfb19b43207b8b74440497f900d1fabe4
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-26 10:20:56 +0200

    Time: 1 ms (97.82%), Space: 39.2 MB (98.34%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n>0 && Integer.bitCount(n) == 1;
+        return n > 0 && Integer.bitCount(n) == 1;
         
         //https://leetcode.com/problems/power-of-two/discuss/676942/JAVA-or-(-n-and-(-n-1-)-)-Explained-or-2-Line-Solution
         
