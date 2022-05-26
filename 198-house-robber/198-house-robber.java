295171058c26429458363185a3bec55dc2759509
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-26 10:18:16 +0200

    Time: 0 ms (100.00%), Space: 42 MB (20.25%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -4,10 +4,9 @@ class Solution {
         int rob = 0; 
         //max money can get if not rob current house
         int notrob = 0; 
-        int currob = 0;
         for(int num: nums) {
             //if rob current value, previous house must not be robbed
-            currob = notrob + num; 
+            int currob = notrob + num; 
             //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
             notrob = Math.max(notrob, rob); 
             rob = currob;
