66325b87b53e4a0e8824c569eac4d21c71680808
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-05-31 23:23:25 +0200

    Time: 2 ms, Memory: 41.6 MB - LeetHub

---
+++ divide-two-integers/divide-two-integers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+                
+        long division = (long) dividend / (long) divisor;
+        
+        return  division >= Integer.MAX_VALUE 
+            ? Integer.MAX_VALUE  
+            : division <= Integer.MIN_VALUE 
+                ? Integer.MIN_VALUE  
+                : (int) division;
+    }
+}
