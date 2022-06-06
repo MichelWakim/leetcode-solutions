5d8a7871185800de08300da66ac57481a3874192
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-06-06 10:03:12 +0200

    Time: 3 ms (88.10%), Space: 46.7 MB (42.82%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> letters = new HashMap<>();
+//         Map<Character, Integer> letters = new HashMap<>();
         
-        for (char letter: magazine.toCharArray()) 
-            letters.put(letter, letters.getOrDefault(letter, 0) +1);
+//         for (char letter: magazine.toCharArray()) 
+//             letters.put(letter, letters.getOrDefault(letter, 0) +1);
         
-        for (char letter: ransomNote.toCharArray()) {
-            letters.put(letter, letters.getOrDefault(letter, 0) -1);
-            if (letters.getOrDefault(letter, -1) < 0 ) return false;
+//         for (char letter: ransomNote.toCharArray()) {
+//             letters.put(letter, letters.getOrDefault(letter, 0) -1);
+//             if (letters.getOrDefault(letter, -1) < 0 ) return false;
+//         }
+        
+//         return true;
+        
+        int[] alphabetsCounter = new int[26];
+        
+        for (char letter : magazine.toCharArray())
+            alphabetsCounter[letter-'a']++;
+
+        for (char letter : ransomNote.toCharArray()){
+            if (alphabetsCounter[letter-'a'] == 0) return false;
+            alphabetsCounter[letter-'a']--;
         }
-        
         return true;
     }
 }
