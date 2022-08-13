2d9d1a7991e2661e238fad241a45d6dfb350a279
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-08-13 12:06:51 +0200

    Time: 1 ms (87.83%), Space: 42.6 MB (29.45%) - LeetHub

--- 68-text-justification/68-text-justification.java
+++ 68-text-justification/68-text-justification.java
@@ -1,39 +1,33 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> result = new ArrayList<>();
+        List<String> justification  = new ArrayList<>();
 
         for(int i=0; i<words.length; i++) {
             StringBuilder line = new StringBuilder();
             int len = 0;
-            int wlen =0;
-            int start=i;
-            int end = i;
-            while(end<words.length && len + words[end].length() <= maxWidth) {
-                len += words[end].length()+1;
-                wlen += words[end].length();
-                end++;
+            int wordsLen = 0;
+            int leftIdx = i;
+            int rightIdx = i;
+            
+            while(rightIdx < words.length && len + words[rightIdx].length() <= maxWidth) {
+                len += words[rightIdx].length()+1;
+                wordsLen += words[rightIdx].length();
+                rightIdx++;
             }
-            int gaps = end - start - 1;
-            int padding = end == words.length ? gaps : maxWidth-wlen;
+            int gaps = rightIdx - leftIdx - 1;
+            int padding = rightIdx == words.length ? gaps : maxWidth-wordsLen;
 
             int spacing = gaps == 0 ? 1 : padding / gaps;
             int extraSpace = gaps != 0 ? padding % gaps : 0;
-            for(int j=start; j<end; j++) {
-                line.append(words[j]);
-                if(j!= end-1) {
-                    line.append(" ".repeat(spacing));
-                }
-                if(extraSpace>0) {
-                    line.append(" ");
-                    extraSpace--;
-                }
+            while (leftIdx < rightIdx) {
+                line.append(words[leftIdx]);
+                if (leftIdx++ != rightIdx - 1) line.append(" ".repeat(spacing));
+                if (extraSpace-- > 0) line.append(" ");
             }
-            while(line.length() < maxWidth) {
-                line.append(" ");
-            }
-            i = end-1;
-            result.add(line.toString());
+            while(line.length() < maxWidth) line.append(" ");
+            i = rightIdx-1;
+            justification.add(line.toString());
         }
-        return result;
+        return justification;
     }
 }
