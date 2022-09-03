d512c8087be368d35915f3d0bee962efb64498bc
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-09-03 13:35:56 +0200

    Time: 185 ms (34.74%), Space: 97.6 MB (80.06%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -1,28 +1,26 @@
 class Solution {
-public int[] fullBloomFlowers(int[][] flowers, int[] persons) {
-        int n = persons.length;
-        int[] result = new int[n];
+     public int[] fullBloomFlowers(int[][] flowers, int[] persons) { 
+            TreeMap<Integer, Integer> tm = new TreeMap<>();
+            tm.put(0,0);
+            for(int[] f : flowers){
+                tm.put(f[0], tm.getOrDefault(f[0],0) + 1);
+                tm.put(f[1]+1, tm.getOrDefault(f[1]+1,0) - 1);
+            }
 
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-		// See explanation here: https://leetcode.com/problems/my-calendar-iii/discuss/109556/JavaC%2B%2B-Clean-Code
-        for (int[] flower : flowers) {
-            treeMap.put(flower[0], treeMap.getOrDefault(flower[0], 0) + 1);
-			// use end + 1 instead of end
-            treeMap.put(flower[1] + 1, treeMap.getOrDefault(flower[1] + 1, 0) - 1);
+            List<Integer> al = new ArrayList<>();
+            int sum = 0;
+            for(int v : tm.values()){
+                sum += v;
+                al.add(sum);
+            }
+            List<Integer> keys = new ArrayList<>(tm.keySet());
+            int[] ans = new int[persons.length];
+            int ptr = 0;
+            for(int p: persons){
+                int idx = Collections.binarySearch(keys, p);
+                if (idx < 0) idx = -idx-2;
+                ans[ptr++] = al.get(idx);
+            }    
+            return ans;
         }
-
-        TreeMap<Integer, Integer> sum = new TreeMap<>();
-        int prev = 0;
-        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
-            prev += entry.getValue();
-            sum.put(entry.getKey(), prev);
-        }
-
-        for (int i = 0; i < n; i++) {
-            Map.Entry<Integer, Integer> entry = sum.floorEntry(persons[i]);
-			// if entry is null then result[i] = 0
-            if (entry != null) result[i] = entry.getValue();
-        }
-        return result;
-    }
 }
