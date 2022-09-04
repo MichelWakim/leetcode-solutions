5f6c96e5810acb81b2929551c818f92fcbb65bad
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-09-04 15:46:28 +0300

    Time: 28 ms (53.16%), Space: 46.1 MB (31.87%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -2,14 +2,29 @@ class Solution {
     // Explaination 
     // https://leetcode.com/problems/sum-of-subarray-ranges/discuss/1624222/JavaC%2B%2BPython-O(n)-solution-detailed-explanation
     public long subArrayRanges(int[] A) {
+        int n = A.length, j, k;
         long res = 0;
-        for (int i = 0; i < A.length; i++) {
-            int max = A[i], min = A[i];
-            for (int j = i + 1; j < A.length; j++) {
-                max = Math.max(max, A[j]);
-                min = Math.min(min, A[j]);
-                res += max - min;
+        
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] > (i == n ? Integer.MIN_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res -= (long)A[j] * (i - j) * (j - k);
+
             }
+            s.push(i);
+        }
+        
+        s.clear();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] < (i == n ? Integer.MAX_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res += (long)A[j] * (i - j) * (j - k);
+
+            }
+            s.push(i);
         }
         return res;
     }
