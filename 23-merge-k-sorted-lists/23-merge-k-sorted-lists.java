57ec710e03ffc42ebea280998e41ddb6e4093e1b
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-09-05 16:17:49 +0300

    Time: 3 ms (90.41%), Space: 47.2 MB (58.34%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int k = lists.length;
+        
+        if( k == 0 ) return null;
+        
+        int step = 1;
+        
+        while ( step < k ) {
+            for ( int i = 0; i < k - step ; i += (step * 2) )
+                lists[i] = this.merge2Lists(lists[i] , lists[i + step]);
+            step *= 2;
+        }
+                
+        return lists[0];
+    }
+    
+    private ListNode merge2Lists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // At least one of l1 and l2 can still have nodes at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+    
+    
+}
