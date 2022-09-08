e2c9c10ba63f04382f821b48864d3937af6cecdd
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-09-08 13:43:24 +0300

    Time: 21 ms (84.52%), Space: 45.6 MB (96.98%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,24 +1,43 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> res = new ArrayList<>();
-        TreeMap<String, Integer> map = new TreeMap<>();
-        Arrays.sort(products);
-        List<String> productsList = Arrays.asList(products);
-
-        for (int i = 0; i < products.length; i++) {
-            map.put(products[i], i);
+    // Equivalent code for lower_bound in Java
+    int lower_bound(String[] products, int start, String word) {
+        int i = start, j = products.length, mid;
+        while (i < j) {
+            mid = (i + j) / 2;
+            if (products[mid].compareTo(word) >= 0)
+                j = mid;
+            else
+                i = mid + 1;
         }
-
-        String key = "";
-        for (char c : searchWord.toCharArray()) {
-            key += c;
-            String ceiling = map.ceilingKey(key);
-            String floor = map.floorKey(key + "~");
-            if (ceiling == null || floor == null) break;
-            res.add(productsList.subList(map.get(ceiling), Math.min(map.get(ceiling) + 3, map.get(floor) + 1)));
-        }
-
-        while (res.size() < searchWord.length()) res.add(new ArrayList<>());
-        return res;
+        return i;
     }
-};
+public
+    List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Arrays.sort(products);
+        List<List<String>> result = new ArrayList<>();
+        int start = 0, bsStart = 0, n = products.length;
+        String prefix = new String();
+        for (char c : searchWord.toCharArray()) {
+            prefix += c;
+
+            // Get the starting index of word starting with `prefix`.
+            start = lower_bound(products, bsStart, prefix);
+
+            // Add empty vector to result.
+            result.add(new ArrayList<>());
+
+            // Add the words with the same prefix to the result.
+            // Loop runs until `i` reaches the end of input or 3 times or till the
+            // prefix is same for `products[i]` Whichever comes first.
+            for (int i = start; i < Math.min(start + 3, n); i++) {
+                if (products[i].length() < prefix.length() || !products[i].substring(0, prefix.length()).equals(prefix))
+                    break;
+                result.get(result.size() - 1).add(products[i]);
+            }
+
+            // Reduce the size of elements to binary search on since we know
+            bsStart = Math.abs(start);
+        }
+        return result;
+    }
+}
