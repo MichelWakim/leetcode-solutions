b86221ec040f5c388c173ff5c872c753cec576ea
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-09-08 09:57:34 +0300

    Time: 92 ms (71.96%), Space: 48.9 MB (42.23%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.java
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int triangularSum(int[] nums) {
         int n = nums.length - 1;
         
-        while (n >= 1) {
+        while (n > 0) {
             for (int i = 0; i < n; i++) 
                 nums[i] = (nums[i] + nums[i + 1]) % 10;
             n--;
