a1df57652a0222870e70bbd8dec7464ae66c659b
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-09-09 14:41:32 +0300

    Time: 1 ms (97.21%), Space: 42 MB (70.71%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String reorganizeString(String s) {
+        // TC : O(n), SC : O(n)
+
+        // calculate the count of characters
+        int[] hash = new int[26];
+        for(char ch : s.toCharArray())
+            hash[ch-'a']++;
+        
+        // find the max
+        int max = 0;
+        char maxCh = 'a';
+
+        for(int i=0;i<26;i++) {
+            if(hash[i] > max) {
+                max = hash[i];
+                maxCh = (char)(i+'a');
+            }
+        }
+        
+        int n = s.length();
+
+        // check if it is possible to reorganise the string as required
+        if((n+1)/2 < max) return "";
+        
+        char[] result = new char[n];
+        int idx = 0;
+
+        // fill the most frequent char
+        while(max > 0) {
+            result[idx] = maxCh;
+            max--;
+            idx+=2;
+        }
+        hash[maxCh-'a'] = 0;
+        
+        // fill the remaining chars
+        for(int i=0;i<26;i++) {
+            while(hash[i] > 0) {
+                if(idx >= n) idx = 1;
+                result[idx] = (char)(i+'a');
+                hash[i]--;
+                idx+=2;
+            }
+        }
+        
+        return new String(result);
+    }
+}
