8408374c18a453cb7fde723a2b0364ce8d5f8dd5
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2022-10-17 18:34:05 +0200

    Time: 0 ms (100.00%), Space: 40.9 MB (37.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,6 @@
 class Solution {
     public int fib(int n) {
-        int a = 0;
-        int b = 1;
-        if (n < 2) return n;
-        for (int i = 2; i <= n; ++i) {
-            int c = a + b;
-            a = b;
-            b = c;
-        } 
-        return b;
+        double goldenRatio = (1 + Math.sqrt(5)) / 2;
+        return (int) Math.round(Math.pow(goldenRatio, n) / Math.sqrt(5));
     }
 }
